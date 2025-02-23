9452b616d5477fab2012b36df36e03dd41e921f5
Epicder <epicder11@gmail.com>
2025-02-22 23:53:16 -0300

    initial approach

---
+++ effective-logistic/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './components_css/Navbar.css'
+function Navbar() {
+  return (
+    <>
+    <div className='navbar'>
+        <div className='logo'>
+            <img src="" alt="logo" />
+        </div>
+        <ul>
+            <li>Inicio</li>
+            <li>Sobre Nosotros</li>
+            <li>Servicio</li>
+            <li>Contacto</li>
+        </ul>
+    </div>
+    </>
+    
+  )
+}
+
+export default Navbar
