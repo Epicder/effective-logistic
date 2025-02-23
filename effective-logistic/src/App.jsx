def344f470ffd9f7331dbb539fa424da67ba7817
Epicder <epicder11@gmail.com>
2025-02-23 14:45:00 -0300

    Working on

--- effective-logistic/src/App.jsx
+++ effective-logistic/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Main from './components/Main'
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <Main/>
     </>
   )
 }
---
+++ effective-logistic/src/components/Main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './components_css/Main.css'
+
+function Main() {
+  return (
+    <>
+    <div className='content'>
+        <div className='parallax'>
+        <div className='wave'>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#db3636" fill-opacity="1" d="M0,32L48,48C96,64,192,96,288,96C384,96,480,64,576,48C672,32,768,32,864,58.7C960,85,1056,139,1152,165.3C1248,192,1344,192,1392,192L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+        </div>
+        </div>
+
+        <div className='content-text'>
+            <h1>¡Bienvenido a Effective Logistic!</h1>
+            <p>En Effective Logistic nos encargamos de llevar tu mercancía de manera segura y eficiente.</p>
+            <p>Contamos con una amplia gama de servicios y con personal altamente capacitado para brindarte la mejor experiencia.</p>
+            <p>¡Contáctanos y descubre por qué somos la mejor opción para tus envíos!</p>
+            <p>¡Contáctanos y descubre por qué somos la mejor opción para tus envíos!</p>
+            <p>¡Contáctanos y descubre por qué somos la mejor opción para tus envíos!</p>
+            <p>¡Contáctanos y descubre por qué somos la mejor opción para tus envíos!</p>
+            <p>¡Contáctanos y descubre por qué somos la mejor opción para tus envíos!</p>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Main
